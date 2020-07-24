66b3fc531946b7be53dd46140fd79569601f0e6e
Andrii <pyatnitskiy.a@gmail.com>
2020-07-24 15:35:09 +0300

    Fix eventTime deserialization

--- src/main/java/com/codenotfound/model/OrderState.java
+++ src/main/java/com/codenotfound/model/OrderState.java
@@ -3,9 +3,16 @@ package com.codenotfound.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.List;
 
@@ -17,14 +24,14 @@ import java.util.List;
 @ToString
 public class OrderState {
     private int eventId;
-
-//    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-//    private LocalDateTime eventTime;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime eventTime;
     private int subscriberId;
     private int subscriberType;
     private Params params;
-//
+
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
